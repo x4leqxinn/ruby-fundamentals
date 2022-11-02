d16d2d7e9124fe8b0a063315025fe5b441425508
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-02 16:34:07 -0300

    ADD: Hello World!

---
+++ initial.rb
@@ -0,0 +1,13 @@
+# print no hace salto de linea
+print("Hello World! \n")
+puts "Hello World!"
+
+# Metodos en ruby
+def say_hello(thing_to_say)
+    puts thing_to_say
+end
+
+say_hello("Primer metodo en ruby")
+
+say_hello "Hello world Ruby is great!"
+
