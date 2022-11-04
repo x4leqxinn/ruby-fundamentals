31853a53bf2bf594a5ddd4ea8421fd66ca157fa1
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-04 14:25:20 -0300

    ADD: Ruby Convention

---
+++ ruby-convention.rb
@@ -0,0 +1,18 @@
+this_is_snake_case
+
+also_snake_case
+
+# El código ruby por conveción se debe escribir en
+# snake case para variables y métodos
+# se debe identar con 2 espacios
+# y debe tener una sangria
+def my_name_is
+  # and my code is here
+
+end
+
+# Nombres de clases como CamelCase
+CamelCase
+
+class SomeClass
+end
